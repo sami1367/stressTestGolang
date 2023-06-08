54abbd46019319ef2503b7ae0bad4c483de8d266
Sami <sjd.rhm@gmail.com>
2023-06-08 09:06:45 +0330

    create for loop

--- stress.go
+++ stress.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"os"
 	"strconv"
 	"sync"
 
@@ -33,32 +32,45 @@ func safarmarketTest(c *gin.Context) {
 
 	wg := sync.WaitGroup{}
 
-	for i := 0; i < intLimit; i++ {
-		wg.Add(1)
-		go callApi(i, &wg)
+	domesticAirports := [3]string{"THR", "MHD", "KIH"}
+	internationalAirports := [3]string{"IKA", "MHD", "ISF"}
+
+	for i := 0; i < intLimit; {
+		for n := 0; n < len(domesticAirports); n++ {
+			i++
+			wg.Add(1)
+			fmt.Println("tt -> ", n, domesticAirports[n])
+			go callApi(i, &wg, "https://api.pateh.com", "THR", "KIH", "2023-06-08")
+		}
+		for m := 0; m < len(internationalAirports); m++ {
+			i++
+			wg.Add(1)
+			fmt.Println("rr -> ", m, internationalAirports[m])
+		}
 	}
 	c.JSON(http.StatusOK, gin.H{"success": true})
-	wg.Wait()
+	wg.Done()
 }
 
-func callApi(i int, wg *sync.WaitGroup) {
+func callApi(i int, wg *sync.WaitGroup, baseUrl string, from string, to string, date string) {
 	defer wg.Done()
 	fmt.Println("test", i)
-	resp, err := http.PostForm("https://api.pateh.com/api/safar-market-search",
+	resp, err := http.PostForm(baseUrl+"/api/safar-market-search",
 		url.Values{
-			"from":          {"THR"},
-			"to":            {"KIH"},
-			"departureDate": {"2023-06-08"},
+			"from":          []string{from},
+			"to":            []string{to},
+			"departureDate": []string{date},
 			"adult":         {"1"},
 			"child":         {"0"},
 			"infant":        {"0"},
 		})
 	if err != nil {
 		fmt.Printf("error making http request: %s\n", err)
-		os.Exit(1)
+		//os.Exit(1)
 	}
-	defer resp.Body.Close()
+	//defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
-	_ = body
-	//fmt.Println(string(body))
+	_ = resp
+	//_ = body
+	fmt.Println(string(body))
 }
